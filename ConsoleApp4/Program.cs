a72339beed6cd1c758dbbeb82476a6c70d7489d9
Tural Taghiyev <turalrt@code.edu.az>
2021-11-28 23:20:06 +0400

    first commit

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,15 @@
+﻿int month=1;
+
+if (month >= 1 && month <= 12)
+{
+    if (month >= 3 && month <= 5)
+        Console.WriteLine("yaz");
+    else if (month >= 6 && month <= 8)
+        Console.WriteLine("yay");
+    else if (month >= 9 && month <= 11)
+        Console.WriteLine("payiz");
+    else
+        Console.WriteLine("qis");
+}
+else
+    Console.WriteLine("ayi tezeden daxil edin");
